561b1c164bafef1b1f0e0ca7ea33b0a310c0dca9
kleberson154 <kleberson55@hotmail.com>
2025-06-06 11:06:27 -0300

    Initial commit

---
+++ src/test/java/com/kleberson/apiRestJava/ApiRestJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kleberson.apiRestJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
